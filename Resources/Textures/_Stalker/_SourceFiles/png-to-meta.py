acd085998480212ecc766874316a189514194420
BELNAKBU <eg.ares@mail.ru>
2026-03-29 19:30:59 +0700

    Fix for RSI validation test
    Fix for RSI validation test
    Add licenses for all meta's that dont have it. Delete or remove dead code

--- Resources/Textures/_Stalker/_SourceFiles/png-to-meta.py
+++ Resources/Textures/_Stalker/_SourceFiles/png-to-meta.py
@@ -4,26 +4,26 @@ import tkinter as tk
 from tkinter import simpledialog
 from PIL import Image
 
-# Генерація meta.json
+# Генерация meta.json
 def generate_meta_json(width, height, directions):
-    # Збирає всі png файли в папці
+    # Собирает все png файлы в папке
     png_files = [f for f in os.listdir('.') if f.endswith('.png')]
 
     # Создает список states
     states = []
     for file in png_files:
-        # Перевіряє роздільну здатність зображення
+        # Проверяет разрешение изображения
         with Image.open(file) as img:
             img_width, img_height = img.size
 
         state = {"name": file.replace('.png', '')}
-        # Додає "directions" тільки якщо під час запуску їх зазначено >1 і роздільна здатність файлу не збігається із зазначеною роздільною здатністю
+        # Добавляет "directions" только если при запуске их указано >1 и разрешение файла не совпадает с указанным разрешением
         if (img_width != width or img_height != height) and directions > 1:
             state["directions"] = directions
 
         states.append(state)
 
-    # Генерація заповнення міти
+    # Генерация заполнения меты
     meta = {
         "version": 1,
         "license": "CC-BY-SA-3.0",
@@ -35,21 +35,21 @@ def generate_meta_json(width, height, directions):
         "states": states
     }
 
-    # Збереження JSON
+    # Сохранение JSON
     with open('meta.json', 'w') as json_file:
         json.dump(meta, json_file, indent=4)
 
-# Інтерфейс вся хуйня
+# Интерфейс вся хуйня
 def ask_user_input():
     root = tk.Tk()
-    root.withdraw()  # Приховати сміття
+    root.withdraw()  # Скрыть мусор
 
     # Спросить за базар
-    width = simpledialog.askinteger("Input", "Ширина в пікселях (X):", parent=root, minvalue=1)
-    height = simpledialog.askinteger("Input", "Висота в пікселях (Y):", parent=root, minvalue=1)
-    directions = simpledialog.askinteger("Input", "Кількість напрямків:", parent=root, minvalue=0)
+    width = simpledialog.askinteger("Input", "Ширина в пикселях (X):", parent=root, minvalue=1)
+    height = simpledialog.askinteger("Input", "Высота в пикселях (Y):", parent=root, minvalue=1)
+    directions = simpledialog.askinteger("Input", "Количество направлений:", parent=root, minvalue=0)
 
-    # Закінчити з генерацією
+    # Закончить с генерацией
     if width is not None and height is not None and directions is not None:
         generate_meta_json(width, height, directions)
 
