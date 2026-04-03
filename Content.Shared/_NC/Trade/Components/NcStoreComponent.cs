88dd38501f4d538e6a6fc80b0ac4bdfc7748c49a
CattyLucky <m>
2026-04-03 16:46:06 +0600

    fixs

--- Content.Shared/_NC/Trade/Components/NcStoreComponent.cs
+++ Content.Shared/_NC/Trade/Components/NcStoreComponent.cs
@@ -1,9 +1,11 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared._NC.Trade;
 
 
 public readonly record struct StoreListingKey(StoreMode Mode, string ListingId);
 
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class NcStoreComponent : Component
 {
     public int CatalogRevision;
